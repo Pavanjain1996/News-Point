7a83d4e2bfc797368c75a9e44c5e2dab01bb62da
Pavan Jain <013pavanjain@gmail.com>
2021-03-03 13:50:51 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    url = 'https://www.bbc.com/news'
+    query = requests.get(url)
+    htmlcontent = query.content
+    soup = BeautifulSoup(htmlcontent, 'html.parser')
+    h3 = soup.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
+    para = soup.find_all('p', class_='gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary')
+    links = soup.find_all('a', class_='gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor')
+    content_to_send=[]
+    for i in range(3):
+        content_to_send.append([h3[i].get_text(),para[i+2].get_text(),'https://www.bbc.com/news'+links[i].get('href')])
+    return render_template('index.html', content=content_to_send)
+
+if __name__=='__main__':
+    app.run()
