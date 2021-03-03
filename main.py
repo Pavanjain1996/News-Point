2de754be25665b6d356b3e23c744cc15ddbd89b2
Pavan Jain <013pavanjain@gmail.com>
2021-03-04 00:06:59 +0530

    added files

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from functions import fetchHTML
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    headline = db.Column(db.String(200), nullable=False)
+    image_url = db.Column(db.String(1000), nullable=False)
+    content = db.Column(db.String(400), nullable=False)
+    redirect_url = db.Column(db.String(400), nullable=False)
+    data_from = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return headline
 
 def bbcNews():
     k = 1
