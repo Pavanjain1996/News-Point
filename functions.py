27d0e2dc19966f735dbc296567a90d5e7bcc0266
Pavan Jain <013pavanjain@gmail.com>
2021-03-04 10:49:15 +0530

    Added Comments

--- functions.py
+++ functions.py
@@ -1,14 +1,17 @@
+#importing modules
 import requests
 from bs4 import BeautifulSoup
 from main import Article
 from main import db
 
+#This function is to fetch html data from website
 def fetchHTML(url):
     query = requests.get(url)
     htmlcontent = query.content
     soup = BeautifulSoup(htmlcontent, 'html.parser')
     return soup
 
+#This function will fiter and collect data from html data of BBC website and push into database
 def bbcNews():
     soup = fetchHTML('https://www.bbc.com/news')
     h3 = soup.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
@@ -18,13 +21,14 @@ def bbcNews():
     for i in range(len(image_url)):
         t = image_url[i].findChild().get('data-src')
         image_url[i] = t[:30] + '600' + t[37:]
-    for i in range(4):
+    for i in range(4):               #Pushing data to database
         article = Article.query.filter_by(headline=h3[i].get_text()).all()
-        if article:
+        if article:           #Checking if the data is already present or not
             print('Article Exists')
         else:
             article = Article(headline=h3[i].get_text(),image_url=image_url[i],content=para[i+2].get_text(),redirect_url='https://www.bbc.com'+links[i].get('href'),data_from='BBC News')
             db.session.add(article)
             db.session.commit()
 
+#Laod functions to push data into database
 bbcNews()
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#importing modules
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+#creating and configuring app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
 db = SQLAlchemy(app)
 
+#Creating class
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     headline = db.Column(db.String(200), nullable=False)
@@ -16,10 +19,12 @@ class Article(db.Model):
     def __repr__(self):
         return self.headline
 
+#Url Routes
 @app.route('/')
 def index():
     content = Article.query.all()
     return render_template('index.html', content=content)
 
+#Launching App
 if __name__=='__main__':
     app.run()
