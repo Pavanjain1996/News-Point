40ee47548f3082708c817a80ff9559ccd94bcd6e
Pavan Jain <013pavanjain@gmail.com>
2021-03-04 13:07:26 +0530

    Added Cnn News Script

--- functions.py
+++ functions.py
@@ -4,31 +4,53 @@ from bs4 import BeautifulSoup
 from main import Article
 from main import db
 
-#This function is to fetch html data from website
+#This function will fetch html data from website
 def fetchHTML(url):
     query = requests.get(url)
     htmlcontent = query.content
     soup = BeautifulSoup(htmlcontent, 'html.parser')
     return soup
 
-#This function will fiter and collect data from html data of BBC website and push into database
+#This function will push data into database
+def pushData(headline, image_url, content, redirect_url, data_from):
+    for i in range(len(headline)):
+        article = Article.query.filter_by(headline=headline[i].get_text()).all()
+        if article:
+            print('Article Exists')
+        else:
+            article = Article(headline=headline[i].get_text(),image_url=image_url[i],content=content[i].get_text(),redirect_url=redirect_url[i],data_from=data_from)
+            db.session.add(article)
+            db.session.commit()
+            print('Article Posted')
+
+#This function will filter the data from html data of BBC website
 def bbcNews():
     soup = fetchHTML('https://www.bbc.com/news')
-    h3 = soup.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
-    para = soup.find_all('p', class_='gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary')
-    links = soup.find_all('a', class_='gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor')
+    headline = soup.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
+    content = soup.find_all('p', class_='gs-c-promo-summary gel-long-primer gs-u-mt nw-c-promo-summary')
+    redirect_url = soup.find_all('a', class_='gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor')
     image_url = soup.find_all('div', class_='gs-o-responsive-image gs-o-responsive-image--16by9')
     for i in range(len(image_url)):
         t = image_url[i].findChild().get('data-src')
         image_url[i] = t[:30] + '600' + t[37:]
-    for i in range(4):               #Pushing data to database
-        article = Article.query.filter_by(headline=h3[i].get_text()).all()
-        if article:           #Checking if the data is already present or not
-            print('Article Exists')
-        else:
-            article = Article(headline=h3[i].get_text(),image_url=image_url[i],content=para[i+2].get_text(),redirect_url='https://www.bbc.com'+links[i].get('href'),data_from='BBC News')
-            db.session.add(article)
-            db.session.commit()
+    pushData(headline[:4], image_url[:4], content[2:6], redirect_url[:4], 'BBC News')
 
-#Laod functions to push data into database
-bbcNews()
+#This function will filter the data from html data of CNN Website
+def cnnNews():
+    soup = fetchHTML('https://www.nytimes.com/section/world')
+    h2 = soup.find_all('h2', class_='css-byk1jx e4e4i5l1')
+    headline = [ x.findChild() for x in h2 ]
+    redirect_url = ['https://www.nytimes.com/'+t.get('href') for t in headline]
+    content = soup.find_all('p', class_='css-tskdi9 e4e4i5l4')
+    image_url = soup.find_all('div', class_='css-10wtrbd')
+    image_url = image_url[:2]
+    image_url = [ x.findChild().findChild().findChild().get('src') for x in image_url]
+    pushData(headline[:2], image_url[:2], content[:2], redirect_url[:2], 'CNN News')
+
+#Throwing Everything into database
+def setDatabase():
+    bbcNews()
+    cnnNews()
+
+#Launching
+setDatabase()
